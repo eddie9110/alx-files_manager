cf8d506456d227a5d9e76f7dfc0991929ef5cf3d
eddie9110 <edwinmaregwa@gmail.com>
2024-05-15 02:51:44 -0700

    new routes added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
@@ -9,6 +12,22 @@ router.get('/status', (req, res) => {
 
 router.get('/stats', (req, res) => {
   AppController.getStats(req, res);
+});
+
+router.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
+});
+
+router.get('/connect', (req, res) => {
+  AuthController.getConnect(req, res);
+});
+
+router.get('/disconnect', (req, res) => {
+  AuthController.getDisconnect(req, res);
+});
+
+router.get('/users/me', (req, res) => {
+  UsersController.getMe(req, res);
 });
 
 module.exports = router;
